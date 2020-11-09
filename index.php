e2c10e283a31e74ceda54d6a1b09aea26f14de3d
devmrh <dev.mohammadreza@yahoo.com>
2020-11-09 13:04:31 +0330

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$app_shell = __DIR__.'/mehr.sh';
+
+$client = new stdClass;
+$client->ftp_port = "21";
+$client->ftp_domain = "mehr2.nrp.co";
+$client->ftp_username = "mehr2";
+$client->ftp_password = "IKHSERsXi";
+$client->ftp_directory = "/domains/mehr2.nrp.co/public_html";
+
+
+
+$c = "$app_shell $client->ftp_port $client->ftp_domain $client->ftp_username $client->ftp_password $client->ftp_directory staging";
+
+echo $c;
+return;
+
+$command = escapeshellcmd($c);
+
+$output = exec($command);
+
+// $a = "mehr.sh 21 mehr2.nrp.co mehr2 IKHSERsXi /domains/mehr2.nrp.co/public_html staging";
---
+++ mehr.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#cd /home/mohammadreza/laravel/mehr/public
+#cd /home/mohammadreza/parsdev/public
+export PHPLOY_PORT=$1
+export PHPLOY_HOST=$2
+export PHPLOY_USER=$3
+export PHPLOY_PASS=$4
+export PHPLOY_PATH=$5
+phploy -s $6
